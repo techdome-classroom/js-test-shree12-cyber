0a22ea3bb8399792f303bf498e68fa8dff43a73d
shree sharma <85297469+shree12-cyber@users.noreply.github.com>
2024-05-09 12:59:40 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,22 @@
 const decodeTheRing = function (s, p) {
-
-    // write your code here
-
-  };
   
-  module.exports = decodeTheRing;
+  if (s.length === 0 && p.length === 0) {
+      return true;
+  }
+  
+  if (s.length === 0 || p.length === 0) {
+      return false;
+  }
+  
+
+  if (p[0] === s[0] || p[0] === '?') {
+      
+      return decodeTheRing(s.slice(1), p.slice(1));
+  } else if (p[0] === '*') {
+      return decodeTheRing(s, p.slice(1)) || decodeTheRing(s.slice(1), p);
+  } else {
+      return false;
+  }
+};
+
+module.exports = decodeTheRing;
